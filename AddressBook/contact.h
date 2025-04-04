e9d082c66f41b90cc49f1c5a2ba60bc0da817024
OneVth <onebrotravel@gmail.com>
2025-04-04 19:20:31 +0900

    feat: extract Contact structure to contact.h for data abstraction

---
+++ AddressBook/contact.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define MAX_NAME_LEN 32
+#define MAX_PHONE_LEN 14	// 000-0000-0000 + '\0'
+#define MAXAGE 120
+
+typedef struct _Contact {
+	char name[MAX_NAME_LEN];
+	char phone[MAX_PHONE_LEN];
+	int age;
+} Contact;
