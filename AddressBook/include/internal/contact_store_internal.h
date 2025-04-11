6c30dff4c34714ba776081fe36fa2c8884d29162
OneVth <onebrotravel@gmail.com>
2025-04-11 23:05:10 +0900

    test: Define internal structure for ContactStore_RBT to support unit tests

--- AddressBook/include/internal/contact_store_internal.h
+++ AddressBook/include/internal/contact_store_internal.h
@@ -1,1 +1,19 @@
+// for unit test only
 #pragma once
+
+#include "contact_store.h"
+
+typedef enum {RED, BLACK} COLOR;
+
+typedef struct _RBNode {
+	const Contact* data;
+	COLOR color;
+	struct _RBNode* parent;
+	struct _RBNode* left;
+	struct _RBNode* right;
+} RBNode;
+
+struct _ContactStore_RBT {
+	RBNode* root;
+	RBNode* nil;
+};
